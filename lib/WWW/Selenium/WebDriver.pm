58f19e71fd49bc8318c7b4017da980ab27ce9e03
Matt Phillips <mattp@cpan.org>
2011-05-14 10:04:44 -0400

    Initial commit

--- lib/WWW/Selenium/WebDriver.pm
+++ lib/WWW/Selenium/WebDriver.pm
@@ -2,4 +2,23 @@ use strict;
 use warnings;
 package WWW::Selenium::WebDriver;
 
+use Moose;
+use Net::HTTP::Spore;
+
+has 'client' => (
+    is      => 'ro',
+    isa     => 'Net::HTTP::Spore',
+    lazy    => 1,
+    builder => '_build_client',
+);
+
+has 'x' => ( isa => 'Int', is => 'rw', lazy => 1 );
+has 'y' => ( isa => 'Int', is => 'rw', lazy => 1 );
+
+sub _build_client {
+    return Net::HTTP::Spore->new_from_spec('spore_webdriver.json')
+                           ->enable('Format::JSON');
+}
+
+
 1;
